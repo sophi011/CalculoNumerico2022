3500dad1af78475e8fdfed7e57ee51138fddf3dc
GabiYuri <62964262+GabiYuri@users.noreply.github.com>
2022-06-01 12:10:52 -0300

    create EP2.py

---
+++ EP2.py
@@ -0,0 +1,26 @@
+"""
+Autoria do código
+Gabriela Yuri Ishikawa - NºUSP: 11804098
+Sophia Celine Rafael Alves Pereira - NºUSP: 11803994
+"""
+
+from EP1 import res_sist_linear
+
+
+def mudanca_variavel(a_x, b_x):
+    
+    return a_y, b_y
+
+def integral_gauss(a, b, f, n):
+
+    return res
+
+def integral_dupla(a, b, g, n):
+
+    return res
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
