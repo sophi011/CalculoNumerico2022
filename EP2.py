725db941edd368fde46e63dbab203393d1ead6e7
GabiYuri <gabriela.ishikawa@gmail.com>
2022-06-02 10:37:46 -0300

    estrutura

--- EP2.py
+++ EP2.py
@@ -4,22 +4,35 @@ Gabriela Yuri Ishikawa - NºUSP: 11804098
 Sophia Celine Rafael Alves Pereira - NºUSP: 11803994
 """
 
-from EP1 import res_sist_linear
+def no_peso(n):
+    if n == 6:
+        x = [0.2386191860831969086305017, 0.6612093864662645136613996, 0.9324695142031520278123016]
+        w = []
+    elif n == 8:
+        pass
 
+    return x, w
 
-def mudanca_variavel(a_x, b_x):
+def mudanca_variavel(a, b):
+    # nós são linearmente transportados e os pesos multiplicados por um fator de escala m
     
-    return a_y, b_y
+    return x, m
 
 def integral_gauss(a, b, f, n):
+    # I = w*f(x)
 
     return res
 
 def integral_dupla(a, b, g, n):
+    # usa integral anterior para calcular g
 
     return res
 
 def main():
+    n = int(input("Insira o valor de n: "))
+    # imprimir o valor de n e os valores calculados para cada integral
+
+
 
 
 if __name__ == "__main__":
