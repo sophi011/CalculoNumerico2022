8cad076386be1b57366b84fdcce3b1bc1e446278
sophi011 <sophiaceline@usp.br>
2022-07-11 00:08:22 -0300

    outra formula de produto interno

--- EP3.py
+++ EP3.py
@@ -9,25 +9,28 @@ import EP1
 import EP2
 
 def calcula_produto_interno(f_1, f_2, a, b):
-    # função que calcula o produto interno de duas funções
     g = "("+ f_1 + ")*("+ f_2 + ")"
+    n_integral = 10 #precisão do cálculo da integral
+    res = EP2.calculaIntegralSimples(a, b, g, n_integral)
+    return res
+
+def produtoInternoL(f_1, Derf_1, f_2, Derf_2, k, q, a, b):
+    # função que retorna o produto interno de duas funções de Un
+    g = "(" + str(k) + "*(" + Derf_1 + ")*(" + Derf_2 + ")) + (" + str(q) + "*(" + f_1 + ")*(" + f_2 + "))"
     n_integral = 10 #precisão do cálculo da integral
 
     res = EP2.calculaIntegralSimples(a, b, g, n_integral)
+    print("prod int", res)
     
     return res
 
 def monta_splines(n, L, a=0, b=0):
     h = L/(n+1) # tamanho de cada intervalo dentro de [0, L]
-    vecx = [0]*(n+2)
+    vecx = np.zeros(n+2)
 
     # funcoes da base do espaço que contem a solução procurada
     phiEsq = ["0"]*(n+2)
     phiDir = ["0"]*(n+2)
-    #phiEsqQuad = ["0"]*(n+2)  # produto de phiEsq por phiEsq
-    #phiDirQuad = ["0"]*(n+2)  # produto de phiDir por phiDir
-    #phi_ijEsq = ["0"]*(n+2)  # produto de phi em i por phi em j de xi-1 até xi
-    #phi_ijDir  =["0"]*(n+2)  # produto de phi em i por phi em j de de xi até xi+1
     
     for i in range(n+2):  # definição dos valores dos nós
         vecx[i] = i*h
@@ -40,52 +43,44 @@ def monta_splines(n, L, a=0, b=0):
     for i in range(1, n+1):  # definição da expressão de phi em função de x
         phiEsq[i] = "(x - " + str(vecx[i-1]) + ")/" + str(h)  # vale de xi-1 até xi
         phiDir[i] = "(" + str(vecx[i+1]) + " - x)/" + str(h)  # vale de xi até xi+1
-        #phiEsqQuad[i] = "(x**2 - 2*x*" + str(vecx[i-1]) + " + " + str(vecx[i-1]) + "**2)/" + str(h) + "**2"
-        #phi_ijEsq[i] = "(x**2 - x*" + str(vecx[i-2]) + " - x*" + str(vecx[i-1]) + " + " + str(vecx[i-1]) + "*" + str(vecx[i-2]) + ")/ "+ str(h) + " **2"
-        #phi_ijDir[i] = "(" + str(vecx[i+1]) + "*" + str(vecx[i]) + " -x*" + str(vecx[i]) + " + x**2)/" + str(h) + "**2"
-        #phiDirQuad[i] = "(" + phiDir[i] + ")*(" + phiDir[i] + ")"
-    #phiDir[0] = a
-    #phiDir[n+1] = b
 
-    #phiDirQuad[0] = "(" + phiDir[0] + ")**2"
     print("\n phiEsq: ", phiEsq)
     print("\n phiDir: ", phiDir)
-    #print("\n phiEsqQuad: ", phiEsqQuad)
-    #print("\n phiDirQuad: ", phiDirQuad)
-    #print("\n phi_ijEsq: ", phi_ijEsq)
-    #print("\n phi_ijDir: ", phi_ijDir)
-    return vecx, phiEsq, phiDir
 
-def monta_matriz(n, f, L, a=0, b=0):
+    return vecx, h, phiEsq, phiDir
+
+def monta_matriz(n, f, L, k, q, a=0, b=0):
 
     veca = np.zeros(n)  # diagonal de baixo da matriz tridiagonal do sistema linear
     vecb = np.zeros(n)  # diagonal principal da matriz tridiagonal do sistema linear
     vecc = np.zeros(n)  # diagonal de cima da matriz tridiagonal do sistema linear
     vecd = np.zeros(n)  # vetor do sistema linear
-    n_integral = 10  # precisao do calculo da integral
-    xvec, phiEsq, phiDir = monta_splines(n, L)
+    xvec, h, phiEsq, phiDir = monta_splines(n, L)
     
     for i in range(1, n+1):
         x = xvec[i]
         # calcula cada produto interno para formar a matriz tridiagonal
-        vecb[i-1] = calcula_produto_interno(phiEsq[i], phiEsq[i], xvec[i-1], xvec[i]) + calcula_produto_interno(phiDir[i], phiDir[i], xvec[i], xvec[i+1]) 
-        vecc[i-1] = calcula_produto_interno(phiEsq[i+1], phiEsq[i], xvec[i-1], xvec[i]) + calcula_produto_interno(phiDir[i+1], phiDir[i], xvec[i], xvec[i+1])
+        #vecb[i-1] = calcula_produto_interno(phiEsq[i], phiEsq[i], xvec[i-1], xvec[i]) + calcula_produto_interno(phiDir[i], phiDir[i], xvec[i], xvec[i+1]) 
+        vecb[i-1] = produtoInternoL(phiEsq[i], "1/"+ str(h), phiEsq[i], "1/"+str(h), k, 1, xvec[i-1], xvec[i]) + produtoInternoL(phiDir[i], "(-1)/"+ str(h), phiDir[i], "(-1)/"+str(h), k, 1, xvec[i], xvec[i+1])
+        #vecc[i-1] = calcula_produto_interno(phiEsq[i+1], phiEsq[i], xvec[i-1], xvec[i]) + calcula_produto_interno(phiDir[i+1], phiDir[i], xvec[i], xvec[i+1])
+        vecc[i-1] = produtoInternoL(phiEsq[i+1], "1/"+ str(h), phiEsq[i], "1/"+str(h), k, 1, xvec[i-1], xvec[i]) #+ produtoInternoL(phiDir[i+1], "(-1)/"+ str(h), phiDir[i], "(-1)/"+str(h), k, 1, xvec[i], xvec[i+1])
         vecd[i-1] = calcula_produto_interno(f, phiEsq[i], xvec[i-1], xvec[i]) + calcula_produto_interno(f, phiDir[i], xvec[i], xvec[i+1])
 
     vecc[n-1] = 0
     for i in range(n-1):    
         veca[i+1] = vecc[i]
         
+    print("x: ", xvec)
     print("a: ", veca)
     print("\nb: ", vecb)
     print("\nc: ", vecc)
     print("\nd: ", vecd)
     return veca, vecb, vecc, vecd
     
-def validacao(f, n, L, u, a, b):
-    veca, vecb, vecc, vecd = monta_matriz(n, f, L)
+def validacao(f, n, L, k, q, u, a, b):
+    veca, vecb, vecc, vecd = monta_matriz(n, f, L, k, q)
     alphas = EP1.resolve_vetor(veca, vecb, vecc, vecd)  # resolucao considerando condicoes iniciais nulas -- retorna o vetor de alphas
-    xvec, phiEsq, phiDir = monta_splines(n, L)
+    xvec, h, phiEsq, phiDir = monta_splines(n, L)
     u_calc = np.zeros(n)
     u_aprox = np.zeros(n)
     u_real = np.zeros(n)
@@ -102,24 +97,26 @@ def calcula_erro(u, u_aprox):
     erro_u = np.zeros(len(u))
     for i in range(len(u)):
         erro_u[i] = abs(u_aprox[i] - u[i])
-    return erro_u
+    return max(erro_u)
 
 
 def main():
-    #f = "12 * x * (1-x) - 2"
-    #u = "(x**2) * (1-x)**2"
-    #L = 1
-    #n = 7
-    #u_aprox, u_real = validacao(f, n, L, u, 0, 0)
-    #print("u_real: ", u_real)
-    #print("u_aprox: ", u_aprox)
-    #erro = calcula_erro(u_real, u_aprox)
-#
-    #print("erro: ", erro)
-    print(calcula_produto_interno("x", "x", 0, 1))
+    f = "12 * x * (1-x) - 2"
+    u = "(x**2) * (1-x)**2"
+    L = 1
+    n = 3
+    k = 1
+    q = 0
+    u_aprox, u_real = validacao(f, n, L, k, q, u, 0, 0)
+    print("u_real: ", u_real)
+    print("u_aprox: ", u_aprox)
+    erro = calcula_erro(u_real, u_aprox)
+
+    print("erro: ", erro)
 
 if __name__ == "__main__":
     main()
+    #print(calcula_produto_interno("x", "x", 1, 0))
     #print(calculaIntegralSimples(0, 1, 'xi**2/2', 6))
 
 #monta_matriz(6, "x", 1)
